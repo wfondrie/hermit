caa22c0efddff616dc4172b1edc7e5fc270befa0
wfondrie <wfondrie@umaryland.edu>
2019-01-20 16:16:02 -0800

    made modifications to many things

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,6 @@
 /**
  * Utils
  */
-
-// Throttle
-//
-const throttle = (callback, limit) => {
-  let timeoutHandler = null;
-  return () => {
-    if (timeoutHandler == null) {
-      timeoutHandler = setTimeout(() => {
-        callback();
-        timeoutHandler = null;
-      }, limit);
-    }
-  };
-};
-
 // addEventListener Helper
 //
 const listen = (ele, e, callback) => {
@@ -27,24 +12,6 @@ const listen = (ele, e, callback) => {
 /**
  * Functions
  */
-
-// Auto Hide Header
-//
-let header = document.getElementById('site-header');
-let lastScrollPosition = window.pageYOffset;
-
-const autoHideHeader = () => {
-  let currentScrollPosition = window.pageYOffset;
-  if (currentScrollPosition > lastScrollPosition) {
-    header.classList.remove('slideInUp');
-    header.classList.add('slideOutDown');
-  } else {
-    header.classList.remove('slideOutDown');
-    header.classList.add('slideInUp');
-  }
-  lastScrollPosition = currentScrollPosition;
-}
-
 // Mobile Menu Toggle
 //
 let mobileMenuVisible = false;
@@ -86,11 +53,9 @@ if (header !== null) {
   listen('#img-btn', "click", showImg);
   listen('.bg-img', "click", hideImg);
 
-  window.addEventListener('scroll', throttle(() => {
-    autoHideHeader();
-
     if (mobileMenuVisible == true) {
       toggleMobileMenu();
     }
   }, 250));
 }
+
